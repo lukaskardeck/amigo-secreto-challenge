12c7367e659038f67701e9ac76543e1734e236ae
lukaskardeck <92531036+lukaskardeck@users.noreply.github.com>
2025-02-06 17:50:07 -0300

    feat: implementando função para sortear o amigo

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 let amigos = [];
 
 let inputNome = document.querySelector(".input-name"); // Pega o campo de entrada do nome
-let listaUL = document.querySelector(".name-list"); // Pega a lista desordenada <ul>
+let listaAmigosSelecionados = document.querySelector(".name-list"); // Pega a lista <ul> que vai renderizar os amigos selecionados
+let resultadoSorteio = document.querySelector(".result-list"); // Pega a lista <ul> que vai exibir o amigo sorteado
 
 function atualizarListaDeAmigos() {
-    listaUL.innerHTML = "";  // Limpa a lista antes de atualizar, para evitar duplicação de elementos
+    listaAmigosSelecionados.innerHTML = "";  // Limpa a lista antes de atualizar, para evitar duplicação de elementos
     for (let i = 0; i < amigos.length; i++) {
         let itemDaLista = document.createElement("li"); // Cria o list item (<li>)
         itemDaLista.textContent = amigos[i]; // Adiciona o nome do amigo no <li>
-        listaUL.appendChild(itemDaLista); // Adiciona o <li> dentro da <ul>
+        listaAmigosSelecionados.appendChild(itemDaLista); // Adiciona o <li> dentro da <ul>
     }
 }
 
@@ -20,11 +21,26 @@ function adicionarAmigo() {
         return;
     }
 
-    if (amigos.includes(nomeInserido)) {
+    if (amigos.includes(nomeInserido)) {    // Se o nome do amigo já estiver incluído na lista de amigos...
         alert("Esse nome já está na lista");
         return;
     }
     
     amigos.push(nomeInserido);   // Adiciona o nome na lista de amigos
     atualizarListaDeAmigos();
+}
+
+function sortearAmigo() {
+    if (!amigos.length) {   // Se a lista de amigos estiver vazia...
+        alert("Nenhum amigo foi inserido na lista!");
+        return;
+    }
+
+    resultadoSorteio.textContent = "";
+
+    let indiceAmigoSorteado = Math.floor(Math.random() * amigos.length); // Sorteia um índice aleatório
+    let amigoSorteado = amigos[indiceAmigoSorteado]; // Pega o nome do amigo relacionado ao índice anteriormente sorteado
+    let itemDaLista = document.createElement("li");
+    itemDaLista.textContent = amigoSorteado;
+    resultadoSorteio.appendChild(itemDaLista);
 }
