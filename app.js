faac7574097296a24d6e0455ca5b76f26a413341
lukaskardeck <92531036+lukaskardeck@users.noreply.github.com>
2025-02-06 15:13:57 -0300

    feat: implementando função que atualiza a lista de amigos na página html

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 let amigos = [];
 
 let inputNome = document.querySelector(".input-name"); // Pega o campo de entrada do nome
+let listaUL = document.querySelector(".name-list"); // Pega a lista desordenada <ul>
 
-function limparCampo() {
-    // Limpa o campo de entrada
-    inputNome.value = ""
+function atualizarListaDeAmigos() {
+    listaUL.innerHTML = ""  // Limpa a lista antes de atualizar, para evitar duplicação de elementos
+    for (let i = 0; i < amigos.length; i++) {
+        let itemDaLista = document.createElement("li") // Cria o list item (<li>)
+        itemDaLista.textContent = amigos[i] // Adiciona o nome do amigo no <li>
+        listaUL.appendChild(itemDaLista) // Adiciona o <li> dentro da <ul>
+    }
 }
 
 function adicionarAmigo() {
     let nomeInserido = inputNome.value.trim(); // Pega o texto inserido no campo de entrada, excluindo os "espaços" ao redor do texto
-    limparCampo()
+    inputNome.value = ""
     if (!nomeInserido) {    // Se o campo de entrada estiver vazio...
         alert("Por favor, insira um nome!");
         return;
     }
     
     amigos.push(nomeInserido)   // Adiciona o nome na lista de amigos
-    console.log(amigos)
+    atualizarListaDeAmigos()
 }
