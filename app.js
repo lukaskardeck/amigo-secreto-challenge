9d16d8fd8d0477c34f2db09a1d1659aff56653b3
lukaskardeck <92531036+lukaskardeck@users.noreply.github.com>
2025-02-06 14:52:25 -0300

    feat: implementa uma função para adicionar nomes de amigos na lista

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+let inputNome = document.querySelector(".input-name"); // Pega o campo de entrada do nome
+
+function limparCampo() {
+    // Limpa o campo de entrada
+    inputNome.value = ""
+}
+
+function adicionarAmigo() {
+    let nomeInserido = inputNome.value.trim(); // Pega o texto inserido no campo de entrada, excluindo os "espaços" ao redor do texto
+    limparCampo()
+    if (!nomeInserido) {    // Se o campo de entrada estiver vazio...
+        alert("Por favor, insira um nome!");
+        return;
+    }
+    
+    amigos.push(nomeInserido)   // Adiciona o nome na lista de amigos
+    console.log(amigos)
+}
